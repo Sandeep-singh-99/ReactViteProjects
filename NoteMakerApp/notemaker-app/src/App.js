3e7d9460efce5ecd545e43132ecdbfd98efde957
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-06-15 13:00:13 +0530

    init

--- NoteMakerApp/notemaker-app/src/App.js
+++ NoteMakerApp/notemaker-app/src/App.js
@@ -1,10 +1,17 @@
-
+import React from "react";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import NoteMakerList from "./Components/NoteMakerList";
 
 function App() {
   return (
-    <>
-    <h1>Sandeep Singh</h1>
-    </>
+    <div>
+    <h1 className="flex justify-center items-center text-3xl ">Sandeep Singh</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<NoteMakerList/>}></Route>
+    </Routes>
+    </BrowserRouter>
+    </div>
   );
 }
 
