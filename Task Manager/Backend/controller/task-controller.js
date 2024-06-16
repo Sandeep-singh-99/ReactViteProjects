ae43ed845f0b513461461b7a7bdc0f83add6ffef
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-06-16 17:19:22 +0530

    backend complete

--- Task Manager/Backend/controller/task-controller.js
+++ Task Manager/Backend/controller/task-controller.js
@@ -1,3 +1,5 @@
+const Task = require("../models/task-model")
+
 const home = (req, res) => {
     try {
         res.json("Welcome to Home Page")
@@ -6,6 +8,69 @@ const home = (req, res) => {
     }
 }
 
+// Middleware to fetch a task by ID
+async function funTask(req, res, next) {
+    let task
+    try {
+         task = await Task.findById(req.params.id)
+        if(!task) {
+            return res.status(404).json({message: 'Task not found'})
+        }
+        res.task = task
+        next()
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
 
+const getTask = async (req, res) => {
+    try {
+        const tasks = await Task.find()
+        res.json(tasks)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
 
-module.exports = {home}
+const addTask = async (req, res) => {
+    const task = new Task({
+        title: req.body.title,
+        description: req.body.description,
+        status: req.body.status
+    })
+    try {
+      const newTask = await task.save()
+      res.status(201).json(newTask)  
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+const updateTask =  async (req, res) => {
+    if (req.body.title != null) {
+        res.task.title = req.body.title
+    }
+    if (req.body.description != null) {
+        res.task.description = req.body.description
+    }
+    if (req.body.status != null) {
+        res.task.status = req.body.status
+    }
+    try {
+        const updatedTask = await res.task.save()
+        res.json(updatedTask)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+}
+
+const deleteTask = async (req, res) => {
+    try {
+        await res.task.deleteOne()
+        res.json({message: 'Task Deleted'})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+module.exports = {home, getTask, addTask, funTask, updateTask, deleteTask}
--- Task Manager/Backend/router/task-router.js
+++ Task Manager/Backend/router/task-router.js
@@ -4,4 +4,14 @@ const taskController = require("../controller/task-controller")
 
 router.route("/").get(taskController.home)
 
+router.route("/tasks").get(taskController.getTask)
+
+router.route("/tasks").post(taskController.addTask)
+
+router.route("/tasks/:id").get(taskController.funTask, (req, res) => res.json(res.task))
+
+router.route("/tasks/:id").put(taskController.funTask, taskController.updateTask)
+
+router.route("/tasks/:id").delete(taskController.funTask, taskController.deleteTask)
+
 module.exports = router
