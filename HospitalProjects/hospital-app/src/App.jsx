951636d3e87239fbd51a699645cc39b34e48378b
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-06-18 09:01:05 +0530

    init

--- HospitalProjects/hospital-app/src/App.jsx
+++ HospitalProjects/hospital-app/src/App.jsx
@@ -1,10 +1,14 @@
-
+import {BrowserRouter, Route, Routes, Router} from 'react-router-dom'
 
 function App() {
-
+  const isLinkActive = (path) => window.location.pathname === path
   return (
     <>
-     
+    <BrowserRouter>
+    <Routes>
+
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
