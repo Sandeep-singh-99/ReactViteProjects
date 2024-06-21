7f2454b1ad410acf08b78dd7d28e20b337938c45
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-06-21 17:17:45 +0530

    init

--- HospitalProjects/hospital-app/src/Components/DoctorCard.jsx
+++ HospitalProjects/hospital-app/src/Components/DoctorCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function DoctorCard({doctor, onEdit, onDelete}) => {
+export default function DoctorCard({doctor, onEdit, onDelete}) {
   return (
     <div>DoctorCard</div>
   )
