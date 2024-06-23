2e98ecb240b260288a4f9ea54173a24d6bd04d85
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-06-23 21:47:56 +0530

    delete

--- QuizApp/Backend/controller/home-controller.js
+++
@@ -1,9 +0,0 @@
-const home = async (req, res) => {
-    try {
-        res.json('Welcome to Home Page')
-    } catch (error) {
-        res.json(error)
-    }
-}
-
-module.exports = {home}
--- QuizApp/Backend/router/home-router.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const homeController = require('../controller/home-controller')
-
-router.route("/").get(homeController.home)
-module.exports = router
