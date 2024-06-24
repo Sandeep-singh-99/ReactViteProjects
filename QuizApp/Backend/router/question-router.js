c81fa8e40943be66c54367f8d92ac62262aa1267
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-06-24 17:21:18 +0530

    delete

--- QuizApp/Backend/router/question-router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const questionController = require("../controller/question-controller")
-
-router.route('/questions').get(questionController.getQuestion).post(questionController.insertQuestion).delete(questionController.dropQuestion)
-
-module.exports = router
--- QuizApp/Backend/router/result-router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const resultController = require("../controller/result-controller")
-
-router.route("/result").get(resultController.getResult).post(resultController.storeReult).delete(resultController.dropResult)
-
-module.exports = router
