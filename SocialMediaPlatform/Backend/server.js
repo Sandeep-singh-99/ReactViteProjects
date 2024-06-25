3809c63309fa113bd8bb2284a8871a275b5ad050
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-06-25 19:48:34 +0530

    database connect

--- SocialMediaPlatform/Backend/server.js
+++ SocialMediaPlatform/Backend/server.js
@@ -2,11 +2,28 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path')
+const multer = require('multer')
+const ConnectDB = require('./utils/db')
 
 app.use(express.json())
 app.use(cors())
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({storage: storage})
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`server is running http://localhost:${process.env.PORT}`);
+ConnectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`server is running http://localhost:${process.env.PORT}`);
+    })
 })
