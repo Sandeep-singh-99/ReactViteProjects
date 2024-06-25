31b74f17e6d9c967b955e4050459fe86b3cffec1
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-06-25 20:48:43 +0530

    router

--- SocialMediaPlatform/Backend/router/social-router.js
+++ SocialMediaPlatform/Backend/router/social-router.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const postController = require('../controller/social-controller')
 
+router.route('/').get(postController.getAllPost)
+
+router.route('/').post(postController.upload.single('file'), postController.createPost)
+
+router.route('/like/:postId').post(postController.likePost)
+
+router.route('/comment/:postId').post(postController.commentPost)
 
 
 module.exports = router
