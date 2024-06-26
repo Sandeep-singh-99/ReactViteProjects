5c929fec716540dd31700e1853490643cb29025f
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-06-26 18:30:35 +0530

    complete

--- Auth/auth-app/src/App.js
+++ Auth/auth-app/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="max-w-4xl mx-auto mt-5">
+      <div>
+        <nav className="bg-black rounded-md text-white p-5">
+          <ul className="flex gap-x-5">
+            <li>
+              <Link to={"/login"}>Login</Link>
+            </li>
+            <li>
+              <Link to={"/register"}>Register</Link>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <Routes>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+          </Routes>
+        </div>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ Auth/auth-app/src/Pages/Register.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Register = () => {
+  const [formData, setFormData] = useState({ name: '', email: '', password: '' });
+
+  const { name, email, password } = formData;
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    const newUser = { name, email, password };
+
+    try {
+      const res = await axios.post('http://localhost:5000/api/auth/register', newUser);
+      console.log(res.data);
+    } catch (err) {
+      console.error(err.response.data);
+    }
+  };
+
+  return (
+    <div className='shadow-lg rounded-md bg-white mt-5 p-7'>
+      <h1 className='text-lg pb-5 font-semibold'>Register</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+        type="text" 
+        name="name" 
+        value={name} 
+        onChange={onChange} 
+        placeholder="Name" 
+        required
+        className='block outline-none w-full border-2 px-3 py-2 rounded-md mb-5' 
+        />
+
+        <input 
+        type="email" 
+        name="email" 
+        value={email} 
+        onChange={onChange}
+        placeholder="Email" 
+        required
+        className='block outline-none w-full border-2 px-3 py-2 rounded-md mb-5' 
+        />
+        <input 
+        type="password" 
+        name="password" 
+        value={password} 
+        onChange={onChange} 
+        placeholder="Password" 
+        required 
+         className='block outline-none w-full border-2 px-3 py-2 rounded-md mb-5'
+        />
+        <button className='bg-blue-500 text-white px-2 py-1 w-[150px] rounded-md' type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
