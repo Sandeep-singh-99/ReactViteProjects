9abce29e9cf025ff7cc2f4d922d311ab24ac3728
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-07-10 16:23:55 +0530

    init

--- NewsApp/news-app/src/Components/Everything/EverythingItem.js
+++ NewsApp/news-app/src/Components/Everything/EverythingItem.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function NewItem({article}) {
   return (
-    <div className='p-20 '>
-       <div className='shadow-lg rounded-lg bg-gray-200 px-5 pt-10'>
+    <div className='p-20'>
+       <div className='shadow-lg rounded-lg bg-gray-200 px-5 pt-10 w-[500px] space-y-5'>
+        {console.log("Api request")}
        <h1 className='text-4xl'>{article.title}</h1>
         <p className='text-xl'>{article.description}</p>
         <img className='h-[80%]' src={article.urlToImage} alt='not showing'/>
