61b7e1661eb8b75f9c2e6d87a866b5c7f8656c8f
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-07-10 16:49:16 +0530

    init

--- NewsApp/news-app/src/Components/Everything/EverythingItem.js
+++ NewsApp/news-app/src/Components/Everything/EverythingItem.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
-function NewItem({article}) {
+function NewItem({ article }) {
   return (
-    <div className='p-20'>
-       <div className='shadow-lg rounded-lg bg-gray-200 px-5 pt-10 w-[500px] space-y-5'>
+    <div className="p-20 bg-slate-950">
+      <div className="shadow-lg rounded-lg bg-gray-200 px-5 pt-10 pb-5  space-y-5">
         {console.log("Api request")}
-       <h1 className='text-4xl'>{article.title}</h1>
-        <p className='text-xl'>{article.description}</p>
-        <img className='h-[80%]' src={article.urlToImage} alt='not showing'/>
-        <p className=''>{article.content}</p>
-       </div>
+        <h1 className="text-4xl">{article.title}</h1>
+        <p className="text-xl">{article.description}</p>
+        <img className="h-[80%]" src={article.urlToImage} alt="not showing" />
+        <p className="">{article.content}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default NewItem
+export default NewItem;
--- NewsApp/news-app/src/Components/Everything/EverythingList.js
+++ NewsApp/news-app/src/Components/Everything/EverythingList.js
@@ -1,44 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
-import NewItem from './EverythingItem'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import NewItem from "./EverythingItem";
 function NewList() {
-    const [article, setArticle] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
+  const [article, setArticle] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchNews = async () => {
-            try {
-                const response = await axios.get("http://localhost:5000/api/news/everything", {
-                    params: {q: "technology", apiKey: "47b8b58359314c5f82b38fe409843466"}
-                });
-                setArticle(response.data)
-                setLoading(false)
-                console.log("success: ", response.data);
-            } catch (error) {
-                setError(error)
-                setLoading(false)
-                console.log("Error", error);
-            }
-        }
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/news/everything",
+          {
+            params: {
+              q: "technology",
+              apiKey: "47b8b58359314c5f82b38fe409843466",
+            },
+          }
+        );
+        setArticle(response.data);
+        setLoading(false);
+        console.log("success: ", response.data);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+        console.log("Error", error);
+      }
+    };
 
-        fetchNews()
-    },[])
+    fetchNews();
+  }, []);
 
-    if (loading) 
-        return <div>Loading...</div>
+  if (loading) return <div>Loading...</div>;
 
-    if (error) 
-        return <div>Error Loading articles: {error.message}</div>
+  if (error) return <div>Error Loading articles: {error.message}</div>;
   return (
     <div>
-        <div>
-            {article.map((articles, index) => (
-                <NewItem key={index} article={articles} />
-            ))}
-        </div>
+      <div className="bg-slate-950">
+        <h1 className="flex justify-center items-center pt-10 text-4xl font-semibold text-white">
+          News App
+        </h1>
+        {article.map((articles, index) => (
+          <NewItem key={index} article={articles} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default NewList
+export default NewList;
