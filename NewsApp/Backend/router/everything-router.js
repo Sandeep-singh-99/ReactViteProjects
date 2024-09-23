5e9e88473d4915b843794cf1c3fd6e5e5aa444d7
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2024-09-23 21:12:23 +0530

    del

--- NewsApp/Backend/router/everything-router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const everythingController = require("../controller/everything-controller")
-
-router.route("/everything").get(everythingController.getAll)
-
-module.exports = router
