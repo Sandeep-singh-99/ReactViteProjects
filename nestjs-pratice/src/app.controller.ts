0b1e799bdc27b9f925ecdf58b5f259c7eb3d9164
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2026-08-09 22:07:12 +0530

    feat: update AppController and AppService to enhance API structure and responses

--- nestjs-pratice/src/app.controller.ts
+++ nestjs-pratice/src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/desc")
+  getDescription(): string {
+    return this.appService.getDescription();
   }
 }
--- nestjs-pratice/src/app.service.ts
+++ nestjs-pratice/src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! Sandeep';
+  }
+
+  getDescription(): string {
+    return "This structure will be especially useful when you start converting your existing microservices/backend experience to NestJS."
   }
 }
